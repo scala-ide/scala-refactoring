82eb954a458187b891cc375be988ce543c4028e1
Mirko Stocker <me@misto.ch>
2010-04-09 10:19:25 +0200

    reorganize method, move definitions closer to their usages

--- src/scala/tools/refactoring/implementations/OrganizeImports.scala
+++ src/scala/tools/refactoring/implementations/OrganizeImports.scala
@@ -24,41 +24,42 @@ abstract class OrganizeImports extends MultiStageRefactoring {
     
   def perform(selection: Selection, prepared: PreparationResult, params: RefactoringParameters): Either[RefactoringError, TreeModifications] = {
     
-    val sortImports: List[Tree] => List[Tree] = _.sortWith({
-      case (t1: Import, t2: Import) => t1.expr.toString < t2.expr.toString
-    }: (Tree, Tree) => Boolean)
-    
-    val collapseImports: List[Tree] => List[Tree] = _.foldRight(List[Import]()) { 
-      case (imp: Import, Nil) => imp :: Nil
-      case (imp: Import, x :: xs) if imp.expr.toString == x.expr.toString => x.copy(selectors = x.selectors ::: imp.selectors).setPos(x.pos) :: xs
-      case (imp: Import, xs) => imp :: xs
-    }
-    
-    val simplifyWildcards: List[Tree] => List[Tree] = {
-      def importsAll(i: ImportSelector) = i.name == nme.WILDCARD
-      def renames(i: ImportSelector) = i.name != i.rename
-      
-      _ map {
-        case imp @ Import(_, selectors) if selectors.exists(importsAll) && !selectors.exists(renames) => 
-          imp.copy(selectors = selectors.filter(importsAll)).setPos(imp.pos)
-        case imp => 
-          imp
-      }
-    }
-    
     var changes = new ModificationCollector {
+      
       transform(selection.file) {
-        case p @ PackageDef(_, stats) => {
-          
-          val sorted = stats partition {
-            case _: Import => true
-            case _ => false
-          } match {
-            case (imports, others) => (sortImports andThen collapseImports andThen simplifyWildcards apply imports) ::: others
-          }
-          
-          p copy (stats = sorted) setPos p.pos
-        }
+        case p @ PackageDef(_, stats) =>
+        
+          p copy (stats = stats partition {
+              case _: Import => true
+              case _ => false
+            } match {
+              case (imports, others) => 
+              
+                val sortImports: List[Tree] => List[Tree] = _.sortBy {
+                  case t: Import => t.expr.toString
+                }
+                  
+                val collapseImports: List[Tree] => List[Tree] = _.foldRight(Nil: List[Import]) { 
+                  case (imp: Import, x :: xs) if imp.expr.toString == x.expr.toString => 
+                    x.copy(selectors = x.selectors ::: imp.selectors).setPos(x.pos) :: xs
+                  case (imp: Import, xs) => 
+                    imp :: xs
+                }
+                
+                val simplifyWildcards: List[Tree] => List[Tree] = {
+                  def importsAll(i: ImportSelector) = i.name == nme.WILDCARD
+                  def renames(i: ImportSelector) = i.name != i.rename
+                  
+                  _ map {
+                    case imp @ Import(_, selectors) if selectors.exists(importsAll) && !selectors.exists(renames) => 
+                      imp.copy(selectors = selectors.filter(importsAll)).setPos(imp.pos)
+                    case imp =>
+                      imp
+                  }
+                }
+                ((sortImports andThen collapseImports andThen simplifyWildcards) apply imports) ::: others
+            }
+          ) setPos p.pos
       }
     }
     
