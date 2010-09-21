ac73f9bc400ba09fbb19ddd815184b8e8984af25
Miles Sabin <miles@milessabin.com>
2010-09-21 17:35:47 +0100

    Updates to support building relative to 2.8.0.RC1.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 mvn -Dscala.version=2.8.0 clean package
+#mvn -Dscala.version=2.8.1.RC1 clean package
 #mvn -Dscala.version=2.8.1-SNAPSHOT clean package
 #mvn -Dscala.version=2.9.0-SNAPSHOT -Dmaven.test.skip=true clean package
