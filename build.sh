046691948a9d829b48dcd3f83e8e91677401548c
Mirko Stocker <me@misto.ch>
2011-02-20 12:16:04 +0100

    cleanup in the build script

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-#mvn -Dscala.version=2.8.0 clean package -Dmaven.test.skip=true $*
+# mvn -Dscala.version=2.8.0 clean package $*
 mvn -Dscala.version=2.8.1 clean package $*
-#mvn -Dscala.version=2.8.1-SNAPSHOT clean package
-#mvn -Dscala.version=2.9.0-SNAPSHOT -Dmaven.test.skip=true clean package
+# mvn -Dscala.version=2.9.0-SNAPSHOT clean package
