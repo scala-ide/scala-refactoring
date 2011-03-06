22dd7e8a2aa0ec4c6ff23015db926415104b0e50
Mirko Stocker <me@misto.ch>
2011-03-06 15:47:51 +0100

    Add a test suite that holds all the refactoring tests, seems to work better for running inside the IDE.

---
+++ org.scala-refactoring.library/src/test/scala/scala/tools/refactoring/tests/RefactoringTestSuite.scala
@@ -0,0 +1,39 @@
+package scala.tools.refactoring
+package tests
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import analysis._
+import common._
+import implementations._
+import sourcegen._
+import transformation._
+import util._
+
+@RunWith(value = classOf[Suite])
+@Suite.SuiteClasses(value = Array(
+    classOf[DeclarationIndexTest], 
+    classOf[MultipleFilesIndexTest], 
+    classOf[NameValidationTest], 
+    classOf[TreeAnalysisTest], 
+    classOf[SelectionsTest],
+    classOf[PimpedTreesTest],
+    classOf[AddImportStatementTest], 
+    classOf[EliminateMatchTest], 
+    classOf[ExplicitGettersSettersTest], 
+    classOf[ExtractLocalTest], 
+    classOf[ExtractMethodTest], 
+    classOf[InlineLocalTest], 
+    classOf[MarkOccurrencesTest], 
+    classOf[OrganizeImportsTest], 
+    classOf[OrganizeMissingImportsTest], 
+    classOf[RenameTest], 
+    classOf[IndividualSourceGenTest], 
+    classOf[LayoutTest], 
+    classOf[PrettyPrinterTest], 
+    classOf[SourceGenTest], 
+    classOf[SourceHelperTest], 
+    classOf[TreeChangesDiscovererTest], 
+    classOf[NotTest], 
+    classOf[TreeTransformationsTest]))
+class RefactoringTestSuite {}
