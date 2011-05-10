d630c504b4ffd9bb94d6852226b4e77bb90de9ca
Mirko Stocker <me@misto.ch>
2011-05-10 07:50:27 +0200

    Change build script to use 2.10 for the master branch.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # mvn -Dscala.version=2.8.0 clean package $*
-mvn -Dscala.version=2.8.1 clean package $*
+# mvn -Dscala.version=2.8.1 clean package $*
 # mvn -Dscala.version=2.9.0-SNAPSHOT clean package
-# mvn -Dscala.version=2.10.0-SNAPSHOT clean package
+mvn -Dscala.version=2.10.0-SNAPSHOT clean package
