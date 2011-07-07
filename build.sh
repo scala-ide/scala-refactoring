f92b45ce36eceac43055a704bbfde8029294a3c7
Miles Sabin <miles@milessabin.com>
2011-07-07 14:10:59 +0100

    Build trunk by default on master.

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 #mvn -Dscala.version=2.8.0 clean package $*
 #mvn -Dscala.version=2.8.1 clean package $*
-mvn -Dscala.version=2.8.2-SNAPSHOT clean package $*
+#mvn -Dscala.version=2.8.2-SNAPSHOT clean package $*
 #mvn -Dscala.version=2.9.0 clean package
 #mvn -Dscala.version=2.9.0-1 clean package
 #mvn -Dscala.version=2.9.1-SNAPSHOT clean package
-#mvn -Dscala.version=2.10.0-SNAPSHOT clean package
+mvn -Dscala.version=2.10.0-SNAPSHOT clean package
