013e2b3d4bc87cf80c19e90ac368a9797167bd55
Mirko Stocker <me@misto.ch>
2012-01-09 14:53:24 +0100

    Better handling of Java annotation-parameters.

--- org.scala-refactoring.library/src/main/scala/scala/tools/refactoring/common/TreeTraverser.scala
+++ org.scala-refactoring.library/src/main/scala/scala/tools/refactoring/common/TreeTraverser.scala
@@ -92,12 +92,28 @@ trait TreeTraverser {
     
     def handleAnnotations(as: List[AnnotationInfo]) {
       as foreach { annotation =>
+
         annotation.atp match {
           case tpe @ TypeRef(_, sym, _) if annotation.pos != NoPosition =>
             val tree = fakeSelectTreeFromType(tpe, sym, annotation.pos)
             traverse(tree)
-          case _ => 
+          case _ =>
         }
+
+        // Annotations with parameters defined in Java need this:
+        annotation.assocs.unzip._2 collect {
+          case LiteralAnnotArg(Constant(value)) => value
+        } foreach {
+          case tpe @ TypeRef(_, sym, _) =>
+            /* The `annotation.pos` is wrong, we should instead extract
+             * the correct positions from the source code. */
+            fakeSelectTreeFromType(tpe, sym, annotation.pos) match {
+              case t: Select => traverse(t)
+              case _ => ()
+            }
+          case _ => ()
+        }
+
         annotation.args foreach traverse
       }
     }
