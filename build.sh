17de4a9491ddff579c0f51cf21ca0602c63c2973
Mirko Stocker <me@misto.ch>
2013-01-21 19:40:22 +0100

    Don't sign the artifacts per default
    Instead, use the release-sign-artifacts profile to sign.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# To sign the artifacts, add the release-sign-artifacts profile to the command
+
 #mvn -Dscala.version=2.9.3-SNAPSHOT  -P scala-2.9.x  clean package $*
 #mvn -Dscala.version=2.10.0-SNAPSHOT -P scala-2.10.x clean package $*
 #mvn -Dscala.version=2.10.1-SNAPSHOT -P scala-2.10.x clean package $*
