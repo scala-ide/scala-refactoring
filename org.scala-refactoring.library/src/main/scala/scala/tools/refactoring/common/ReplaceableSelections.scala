23c8c6d559e664fffca0b80b7943e7d0ce7db9ed
Luegg <wegmaluk@gmail.com>
2013-11-03 15:09:29 +0100

    introduce replaceable selections extensions

--- org.scala-refactoring.library/src/main/scala/scala/tools/refactoring/common/ReplaceableSelections.scala
+++ org.scala-refactoring.library/src/main/scala/scala/tools/refactoring/common/ReplaceableSelections.scala
@@ -1,8 +1,9 @@
 package scala.tools.refactoring.common
 
 import scala.reflect.internal.util.RangePosition
+import scala.tools.refactoring.transformation.TreeTransformations
 
-trait ReplaceableSelections extends Selections {
+trait ReplaceableSelections extends Selections with TreeTransformations {
   self: CompilerAccess =>
 
   import global._
@@ -41,23 +42,74 @@ trait ReplaceableSelections extends Selections {
       if (selectionMatchesFirstTree) {
         Some(selection)
       } else {
-        selection.findSelectedOfType[Tree].flatMap { enclosing =>
-          def posOfPartiallySelectedTrees(trees: List[Tree], newPos: Position = selection.pos): Position =
-            trees match {
-              case t :: rest if !t.pos.isRange || t.pos.end < selection.pos.start || t.pos.start > selection.pos.end =>
-                posOfPartiallySelectedTrees(rest, newPos)
-              case t :: rest =>
-                posOfPartiallySelectedTrees(rest, newPos union t.pos)
-              case Nil => newPos
-            }
+        def posOfPartiallySelectedTrees(trees: List[Tree], newPos: Position = selection.pos): Position =
+          trees match {
+            case t :: rest if !t.pos.isRange || t.pos.end < selection.pos.start || t.pos.start > selection.pos.end =>
+              posOfPartiallySelectedTrees(rest, newPos)
+            case t :: rest =>
+              posOfPartiallySelectedTrees(rest, newPos union t.pos)
+            case Nil => newPos
+          }
 
-          expandTo(posOfPartiallySelectedTrees(enclosing.children))
-        }
+        expandTo(posOfPartiallySelectedTrees(selection.enclosingTree.children))
       }
-      
+
+  }
+
+  implicit class SelectionProperties(selection: Selection) {
+    lazy val definesNonLocal = selection.selectedTopLevelTrees.exists(cond(_) {
+      case t: DefTree => !t.symbol.isLocal
+    })
+
+    lazy val definesNonValue = selection.selectedTopLevelTrees.exists(cond(_) {
+      case t: DefTree => !t.symbol.isType
+    })
+
+    lazy val enclosingTree =
+      selection.findSelectedWithPredicate {
+        case t => t != selection.selectedTopLevelTrees.head
+      }.getOrElse(selection.root)
   }
 
   implicit class ReplaceableSelection(selection: Selection) {
+    private def descendToEnclosingTreeAndThen(trans: Transformation[Tree, Tree]) =
+      topdown {
+        matchingChildren {
+          predicate { (t: Tree) =>
+            t == selection.enclosingTree
+          } &> trans
+        }
+      }
+
+    private def replaceSingleStatementBy(replacement: Tree) = {
+      val substitution = replaceTree(selection.selectedTopLevelTrees.head, replacement)
+      substitution |> topdown {
+        matchingChildren {
+          substitution
+        }
+      }
+    }
+
+    private def replaceSequenceBy(replacement: Tree) = {
+      transform {
+        case block @ Block(stats, expr) =>
+          val allStats = (stats :+ expr)
+          if (allStats.length == selection.selectedTopLevelTrees.length) {
+            replacement replaces block
+          } else {
+            val newStats = (stats :+ expr).replaceSequence(selection.selectedTopLevelTrees, replacement :: Nil)
+            mkBlock(newStats) replaces block
+          }
+      }
+    }
+
+    def replaceBy(replacement: Tree) =
+      descendToEnclosingTreeAndThen {
+        if (selection.selectedTopLevelTrees.length == 1)
+          replaceSingleStatementBy(replacement)
+        else
+          replaceSequenceBy(replacement)
+      }
 
   }
 }
---
+++ org.scala-refactoring.library/src/test/scala/scala/tools/refactoring/tests/common/ReplaceableSelectionTest.scala
@@ -0,0 +1,83 @@
+package scala.tools.refactoring.tests.common
+
+import scala.tools.refactoring.tests.util.TestHelper
+import scala.tools.refactoring.common.ReplaceableSelections
+import org.junit.Assert._
+
+class ReplaceableSelectionTest extends TestHelper with ReplaceableSelections {
+  import global._
+
+  val t123 = Literal(Constant(123))
+  val tprint123 =
+    "object O { /*(*/println(123)/*)*/ }".selection.selectedTopLevelTrees.head
+
+  implicit class StringToSel(src: String) {
+    val root = treeFrom(src)
+    val selection = {
+      val start = commentSelectionStart(src)
+      val end = commentSelectionEnd(src)
+      FileSelection(root.pos.source.file, root, start, end)
+    }
+
+    def assertReplacement(mkTrans: Selection => Transformation[Tree, Tree]) = {
+      val trans = mkTrans(selection)
+      val result = trans(root)
+
+      new {
+        def toFail =
+          assertTrue(result.isEmpty)
+
+        def toBecome(expectedSrc: String) = {
+          val (expected, actual) = global.ask { () =>
+            (expectedSrc.root.toString(), result.get.toString())
+          }
+          assertEquals(expected, actual)
+        }
+      }
+    }
+  }
+
+  @Test
+  def replaceSingleStatement = """
+    object O{
+      def f = /*(*/1/*)*/
+    }
+    """.assertReplacement(_.replaceBy(t123)).toBecome("""
+    object O{
+      def f = 123
+    }
+    """)
+
+  @Test
+  def replaceSequence = """
+    object O{
+      def f = {
+        /*(*/println(1)
+        println(2)/*)*/
+        println(3)
+      }
+    }
+    """.assertReplacement(_.replaceBy(t123)).toBecome("""
+    object O{
+      def f = {
+        123
+        println(3)
+      }
+    }
+    """)
+
+  @Test
+  def replaceAllExpressionsInBlock = """
+    object O{
+      def f = {
+        /*(*/println(1)
+        println(2)
+        println(3)/*)*/
+      }
+    }
+    """.assertReplacement(_.replaceBy(tprint123)).toBecome("""
+    object O{
+      def f = println(123)
+    }
+    """)
+}
