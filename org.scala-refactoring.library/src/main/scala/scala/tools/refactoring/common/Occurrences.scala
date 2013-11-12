d3f50574d90b7573b391fd4a1bb9f632321291a5
Luegg <wegmaluk@gmail.com>
2013-11-12 13:39:08 +0100

    add Occurrences

---
+++ org.scala-refactoring.library/src/main/scala/scala/tools/refactoring/common/Occurrences.scala
@@ -0,0 +1,74 @@
+package scala.tools.refactoring.common
+
+import scala.tools.refactoring.analysis.Indexes
+import scala.reflect.internal.util.RangePosition
+import scala.reflect.internal.util.OffsetPosition
+
+/**
+ * Provides functionalities to get positions of term names. This includes the term name
+ * defintion and all its uses.
+ */
+trait Occurrences extends Selections with CompilerAccess with Indexes {
+  import global._
+  
+  type Occurrence = (Int, Int)
+
+  private def termNameDefinition(root: Tree, name: String) = {
+    root.collect {
+      case t: DefTree if t.name.decode == name => 
+        val pos = t.pos
+        t
+    }.headOption
+  }
+  
+  private def defToOccurrence(t: DefTree) = t.namePosition() match {
+    case p: RangePosition => 
+      (p.start, p.end - p.start)
+    case p: OffsetPosition => 
+      (p.point, t.name.decode.length)
+  }
+  
+  private def refToOccurrence(t: RefTree) = t.pos match {
+    case p: RangePosition => 
+      (p.start, p.end - p.start)
+    case p: OffsetPosition => 
+      (p.point, t.symbol.name.decode.length)
+  }
+
+  /**
+   * Returns all uses of the term name introduced by the DefTree t.
+   */
+  def allOccurrences(t: DefTree): List[Occurrence] = {
+    val refOccurrences = index.references(t.symbol).collect {
+      case ref: RefTree => refToOccurrence(ref)
+    }
+    defToOccurrence(t) :: refOccurrences
+  }
+
+  /**
+   * Searches for a definition of `name` in `root` and returns is's position
+   * and all positions of references to the definition.
+   * Returns an empty list if the definition of `name` is not in `selection`. 
+   */
+  def termNameOccurrences(root: Tree, name: String): List[Occurrence] = {
+    termNameDefinition(root, name) match {
+      case Some(t) => allOccurrences(t)
+      case None => Nil
+    }
+  }
+
+  /**
+   * Searches for a method definition of `defName` in `root` and returns for each
+   * method parameter a list with the position of the parameter definition and
+   * all occurrences.
+   * Returns an empty list if `defName` is not found, defines something that is not
+   * a method or if the method has no parameters.
+   */
+  def defDefParameterOccurrences(root: Tree, defName: String): List[List[Occurrence]] = {
+    termNameDefinition(root, defName) match {
+      case Some(DefDef(_, _, _, params, _, _)) =>
+        params.flatten.map { p => allOccurrences(p) }
+      case _ => Nil
+    }
+  }
+}
---
+++ org.scala-refactoring.library/src/test/scala/scala/tools/refactoring/tests/common/OccurrencesTest.scala
@@ -0,0 +1,86 @@
+package scala.tools.refactoring.tests.common
+
+import scala.tools.refactoring.tests.util.TestHelper
+import scala.tools.refactoring.common.Occurrences
+import org.junit.Assert._
+import scala.tools.refactoring.analysis.GlobalIndexes
+
+class OccurrencesTest extends TestHelper with GlobalIndexes with Occurrences {
+  import global._
+
+  var index: IndexLookup = null
+
+  def withIndex(src: String)(body: Tree => Unit) {
+    val tree = treeFrom(src)
+    global.ask { () =>
+      index = GlobalIndex(List(CompilationUnitIndex(tree)))
+    }
+    body(tree)
+  }
+
+  @Test
+  def termOccurrences = {
+    val src = """
+      object O{
+        def fn = {
+          val a = 1
+    	  val b = {
+            val a = 2
+            a
+          }
+          a * a
+        }
+      }
+      """
+    withIndex(src) { root =>
+      val os = global.ask { () => termNameOccurrences(root, "a") }
+      assertEquals(src.indexOf("a = 1"), os.head._1)
+      assertTrue(os.forall(_._2 == "a".length))
+      assertEquals(3, os.length)
+    }
+  }
+
+  @Test
+  @Ignore("fails because of issue https://www.assembla.com/spaces/scala-refactoring/tickets/83")
+  def accessorNameOccurrences = {
+    val src = """
+      object O{
+        val field = 1
+      
+        def fn = 2 * field
+      }
+      """
+    withIndex(src) { root =>
+      val os = global.ask { () => termNameOccurrences(root, "field") }
+      assertEquals(src.indexOf("field"), os.head._1)
+      assertTrue(os.forall(_._2 == "field".length))
+      assertEquals(2, os.length)
+    }
+  }
+
+  @Test
+  @Ignore("fails because of issue https://www.assembla.com/spaces/scala-refactoring/tickets/83")
+  def paramsOccurrences = {
+    val src = """
+      object O{
+        def fn(a: Int, b: Int) = {
+          val c = a * b
+          a * b * c
+        }
+      }
+      """
+    withIndex(src) { root =>
+      val os = global.ask { () => defDefParameterOccurrences(root, "fn") }
+      val aos = os(0)
+      val bos = os(1)
+      assertEquals(src.indexOf("a: Int"), aos.head._1)
+      assertEquals(src.indexOf("b: Int"), bos.head._1)
+      assertTrue((aos ::: bos).forall(_._2 == 1))
+      // two params
+      assertEquals(2, os.length)
+      // each name occurs three times
+      assertEquals(aos.length, 3)
+      assertEquals(bos.length, 3)
+    }
+  }
+}
