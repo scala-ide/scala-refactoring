eb22d70552c32ade3b87b66b7f45fa87762ac718
Mirco Dotta <mirco.dotta@typesafe.com>
2013-12-12 16:11:02 +0100

    `scala.version` property now set in the corresponding profile
    Let's have one single place where the `scala.version` is assigned.
    This is useful also for the build kitty (which I'm setting up for
    validating PRs opened on the scala-refactoring repo).

--- build-2.10.sh
+++ build-2.10.sh
@@ -2,4 +2,4 @@
 
 # To sign the artifacts, add the release-sign-artifacts profile to the command
 
-mvn -Dscala.version=2.10.3-SNAPSHOT -P scala-2.10.x clean package $*
+mvn -P scala-2.10.x clean package $*
