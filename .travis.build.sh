eb903844cf167e525c115519788d8bda8450d217
Luegg <wegmaluk@gmail.com>
2014-02-11 14:23:38 +0100

    remove travis integration

--- .travis.build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-MINOR_VERSION=`echo "$TRAVIS_SCALA_VERSION" | sed 's/^\([0-9]*\)\.\([0-9]*\)\.[^\.]*$/\1.\2/g'`
-
-echo "Build for scala $MINOR_VERSION"
-
-eval "./build-$MINOR_VERSION.sh"
