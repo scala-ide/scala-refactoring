bd925cea82d154aa6d6af8c86b94e3b5fde98a78
Matthias Langer <mlangc@gmx.at>
2015-11-13 15:38:01 +0100

    Don't break code when renaming lazy vals
    Fixes #1002502

--- src/main/scala/scala/tools/refactoring/implementations/Rename.scala
+++ src/main/scala/scala/tools/refactoring/implementations/Rename.scala
@@ -53,6 +53,32 @@ abstract class Rename extends MultiStageRefactoring with TreeAnalysis with analy
   }
 
   def perform(selection: Selection, prepared: PreparationResult, newName: RefactoringParameters): Either[RefactoringError, List[Change]] = {
+    /*
+     * A lazy val is represented by a ValDef and an associated DefDef that contains the initialization code.
+     * Unfortunately, the Scala compiler does not set modifier positions for the DefDef, which is a problem for us,
+     * because we are relying on the DefDef when printing out source code. The following function patches the affected
+     * DefDefs accordingly.
+     *
+     * See #1002392 and #1002502
+     */
+    def eventuallyFixModifierPositionsForLazyVals(t: Tree): Unit = t match {
+      case dd: DefDef if dd.mods.isLazy && dd.mods.positions.isEmpty =>
+        val vd = selection.root.find {
+          case vd: ValDef if vd.mods.isLazy && !vd.mods.positions.isEmpty && dd.pos.point == vd.pos.point => true
+          case _ => false
+        }
+
+        vd.foreach { vd =>
+          // Note that we patch the DefDef in place because we need the correctly set positions in the tree
+          // later in the refactoring process. A precursor of this code pretended to be pure, by copying
+          // dd and then calling 'mods.setPositions' on the copy. This had exactly the same (needed) side effects
+          // however, as the Modifier object affected by setPositions was the same anyway. If you are interested
+          // in the related discussion, take a look at https://github.com/scala-ide/scala-refactoring/pull/78.
+          dd.mods.setPositions(vd.asInstanceOf[ValDef].mods.positions)
+        }
+
+      case _ => ()
+    }
 
     trace("Selected tree is %s", prepared.selectedTree)
 
@@ -60,7 +86,10 @@ abstract class Rename extends MultiStageRefactoring with TreeAnalysis with analy
 
     val occurences = index.occurences(sym)
 
-    occurences foreach (s => trace("Symbol is referenced at %s", PositionDebugging.formatCompact(s.pos)))
+    occurences.foreach { s =>
+      trace("Symbol is referenced at %s", PositionDebugging.formatCompact(s.pos))
+      eventuallyFixModifierPositionsForLazyVals(s)
+    }
 
     val isInTheIndex = filter {
       case t: Tree => occurences contains t
