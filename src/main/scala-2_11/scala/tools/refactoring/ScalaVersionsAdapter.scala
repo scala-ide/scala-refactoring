de8f4ff43eccfa4e1104945d82de6ac205a85b68
Matthias Langer <mlangc@gmx.at>
2015-11-28 16:23:56 +0100

    Fix two problems with printing trees related to default arguments
    Fixes #1002564

--- src/main/scala-2_11/scala/tools/refactoring/ScalaVersionsAdapter.scala
+++ src/main/scala-2_11/scala/tools/refactoring/ScalaVersionsAdapter.scala
@@ -1,10 +1,14 @@
 package scala.tools.refactoring
 
 object ScalaVersionAdapters {
-
-  trait CompilerApiAdapters { self: common.InteractiveScalaCompiler =>
+  trait CompilerApiAdapters {
+    val global: scala.tools.nsc.Global
     import global._
 
     def annotationInfoTree(info: AnnotationInfo): Tree = info.tree
+
+    def isImplementationArtifact(sym: Symbol): Boolean = {
+      sym.isImplementationArtifact
+    }
   }
 }
