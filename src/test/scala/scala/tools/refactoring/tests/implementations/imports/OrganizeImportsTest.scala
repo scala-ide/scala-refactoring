b4cac4859119ae0d7704e449262ce7eea5afb262
Matthias Langer <mlangc@gmx.at>
2015-12-28 11:47:56 +0100

    Add tests for ticket #1002613

--- src/test/scala/scala/tools/refactoring/tests/implementations/imports/OrganizeImportsTest.scala
+++ src/test/scala/scala/tools/refactoring/tests/implementations/imports/OrganizeImportsTest.scala
@@ -845,4 +845,113 @@ class OrganizeImportsTest extends OrganizeImportsBaseTest {
     }
     """
   } applyRefactoring organizeCustomized(groupPkgs = List("java", "scala", "java"))
+
+  /*
+   * See Assembla Ticket 1002613
+   */
+  @Test
+  def organizeImportsRemovesNeededImport1002613Ex1() = new FileSet {
+    """
+    package test
+
+    import java.util.Collections
+
+    class Bug {
+      import Collections.emptyList
+
+      def test = emptyList
+    }
+    """ isNotModified
+  } applyRefactoring organizeWithTypicalParams
+
+  @Test
+  def organizeImportsRemovesNeededImport1002613Ex2() = new FileSet {
+    """
+    package test
+
+    import java.util.Collections
+
+    class Bug {
+      import java.util.Collections.emptyList
+
+      def test = emptyList
+    }
+    """ becomes
+    """
+    package test
+
+    class Bug {
+      import java.util.Collections.emptyList
+
+      def test = emptyList
+    }
+    """
+  } applyRefactoring organizeWithTypicalParams
+
+  @Test
+  def organizeImportsRemovesNeededImport1002613Ex3() = new FileSet {
+    """
+    package test
+
+    import java.util.Collections.emptyList
+
+    class Bug {
+      import java.util.Collections.emptyList
+
+      def test = emptyList
+    }
+    """ becomes
+    """
+    package test
+
+    class Bug {
+      import java.util.Collections.emptyList
+
+      def test = emptyList
+    }
+    """
+  } applyRefactoring organizeWithTypicalParams
+
+  @Test
+  def organizeImportsRemovesNeededImport1002613Ex4() = new FileSet {
+    """
+    package test
+
+    import java.util.Collections
+
+    class Bug {
+      def test = {
+        import Collections.emptyList
+        emptyList
+      }
+    }
+    """ isNotModified
+  } applyRefactoring organizeWithTypicalParams
+
+  @Test
+  def organizeImportsSimilarButNotAffectedBy1002613Ex1() = new FileSet {
+    """
+    package test
+
+    import java.util.Collections.emptyList
+
+    class Bug {
+      import java.util.Collections
+      import Collections.emptyList
+
+      def test = emptyList
+    }
+    """ becomes
+    """
+    package test
+
+    class Bug {
+      import java.util.Collections
+      import Collections.emptyList
+
+      def test = emptyList
+    }
+    """
+  } applyRefactoring organizeWithTypicalParams
+
 }
