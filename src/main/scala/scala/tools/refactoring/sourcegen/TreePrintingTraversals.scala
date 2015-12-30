2c1d184f309be5ceb7870735d6244c681c747619
Simon Schäfer <mail@antoras.de>
2015-12-30 23:42:44 +0100

    Optimize fold logic in printManyTrees

--- src/main/scala/scala/tools/refactoring/sourcegen/TreePrintingTraversals.scala
+++ src/main/scala/scala/tools/refactoring/sourcegen/TreePrintingTraversals.scala
@@ -264,22 +264,22 @@ trait TreePrintingTraversals {
       before: Requisite,
       after: Requisite)(implicit ctx: PrintingContext): Fragment = {
 
-      trees.map { t =>
-        printSingleTree(t, NoRequisite, NoRequisite)
-      }.filterNot(_.isEmpty).foldRight(EmptyFragment: Fragment) {
-        case (l, r) if l.asText == "" => r
-        case (l, r) if r.asText == "" => l
-        case (l, r) =>
+      val fragment = trees.foldRight(EmptyFragment: Fragment) {
+        case (t, r) =>
+          printSingleTree(t, NoRequisite, NoRequisite) match {
+            case l if l.isEmpty || l.asText.isEmpty => r
+            case l if r.asText.isEmpty => l
+            case l =>
+              val leftCenter = balanceBracketsInLayout('(', ')', l.center)
+              val rightCenter = balanceBracketsInLayout('(', ')', r.center)
 
-          val leftCenter = balanceBracketsInLayout('(', ')', l.center)
-          val rightCenter = balanceBracketsInLayout('(', ')', r.center)
-
-          val left = l.post(leftCenter ++ l.trailing, NoLayout)
-          val right = r.pre(NoLayout, r.leading ++ rightCenter)
-          val middle = (left ++ separator ++ right).toLayout
-          val f = Fragment(l.leading, middle, r.trailing) ++ (r.post, l.pre)
-          f
-      } ifNotEmpty (_ ++ (after, before))
+              val left = l.post(leftCenter ++ l.trailing, NoLayout)
+              val right = r.pre(NoLayout, r.leading ++ rightCenter)
+              val middle = (left ++ separator ++ right).toLayout
+              Fragment(l.leading, middle, r.trailing) ++ (r.post, l.pre)
+           }
+      }
+      fragment ifNotEmpty (_ ++ (after, before))
     }
 
     def getChildrenIndentation(parent: Tree, t: Tree): Option[String] = {
