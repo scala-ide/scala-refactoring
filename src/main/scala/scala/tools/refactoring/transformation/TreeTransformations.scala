771ef50f9e3a88db6f75367f7dd3da6d51fd5534
Simon Schäfer <mail@antoras.de>
2016-03-21 12:57:33 +0100

    Add empty lines around added imports and put import at right location
    This commit is doing two things. For every import that is added, it is
    checked that it is surrounded by empty lines if they are necessary.
    "Necessary" means that the empty lines are added in two cases: After a
    package declaration and before a class declaration. Once an import is
    added, a code snippet like the following
    
        package pkg
        trait X {
          def d: Try[Int]
        }
    
    will look like this:
    
        package pkg
    
        import scala.util.Try
    
        trait X {
          def d: Try[Int]
        }
    
    The added empty lines actually complicate the implementation and are not
    needed to fix the problem of the missing import. But they are needed to
    ensure consistency in the implementation. If we have a code snippet like
    the following
    
        package pkg
    
        trait X {
          def d: Try[Int]
        }
    
    The question about where to put the import declaration comes up. Shall
    it be added before or after the empty line? Or shall the empty line even
    be replaced by the import declaration? To me, it looked easier to just
    forget about these questions and instead to ensure that empty lines
    always exist around the import.
    
    In case some imports already exist, there is no need for an import
    declaration:
    
        package pkg
    
        import scala.util.Try
    
        trait X {
          def d: Try[Int]
          def s: Success[Int]
        }
    
    will turn into
    
        package pkg
    
        import scala.util.Try
        import scala.util.Success
    
        trait X {
          def d: Try[Int]
          def s: Success[Int]
        }
    
    without an additional line added before the new import declaration.
    
    The second problem that is fixed by this commit is that the import is
    now added before a comment:
    
        package pkg
        /**
         * Scaladoc
         */
        trait X {
          def d: Try[Int]
        }
    
    will turn into
    
        package pkg
    
        import scala.util.Try
    
        /**
         * Scaladoc
         */
        trait X {
          def d: Try[Int]
        }
    
    Previously it was added after the comment, which is not what anyone
    would expect.
    
    Fixes #1002672

--- src/main/scala/scala/tools/refactoring/transformation/TreeTransformations.scala
+++ src/main/scala/scala/tools/refactoring/transformation/TreeTransformations.scala
@@ -193,7 +193,19 @@ trait TreeTransformations extends Transformations with TreeFactory {
       val line = pos.source.lineToString(lineNumber)
       val indent = line.takeWhile(Character.isWhitespace)
       val insertPos = pos.source.lineToOffset(lineNumber) + line.length
-      TextChange(pos.source, insertPos, insertPos, "\n" + importsAsSrc(indent))
+      val isPkgLine = lineNumber > 0 && pos.source.lineToString(lineNumber-1).matches("""\s*package.*""")
+      val isEmptyLine = pos.source.lineToString(lineNumber+1).trim.isEmpty
+      TextChange(pos.source, insertPos, insertPos, (if (isPkgLine) "\n\n" else "\n")  + importsAsSrc(indent) + (if (isEmptyLine) "" else "\n"))
+    }
+
+    def insertAfterPackage(pos: Position) = {
+      val lineNumber = pos.source.offsetToLine(pos.start)
+      val line = pos.source.lineToString(lineNumber)
+      val indent = line.takeWhile(Character.isWhitespace)
+      val insertPos = pos.source.lineToOffset(lineNumber) + line.length
+      val isPkgLine = line.matches("""\s*package.*""")
+      val isEmptyLine = pos.source.lineToString(lineNumber+1).trim.isEmpty
+      TextChange(pos.source, insertPos, insertPos, (if (isPkgLine) "\n\n" else "\n")  + importsAsSrc(indent) + (if (isEmptyLine) "" else "\n"))
     }
 
     def insertBefore(pos: Position) = {
@@ -201,7 +213,9 @@ trait TreeTransformations extends Transformations with TreeFactory {
       val line = pos.source.lineToString(lineNumber)
       val indent = line.takeWhile(Character.isWhitespace)
       val insertPos = pos.source.lineToOffset(lineNumber)
-      TextChange(pos.source, insertPos, insertPos, importsAsSrc(indent) + "\n")
+      val isPkgLine = lineNumber > 0 && pos.source.lineToString(lineNumber-1).matches("""\s*package.*""")
+      val isEmptyLine = pos.source.lineToString(lineNumber+1).trim.isEmpty
+      TextChange(pos.source, insertPos, insertPos, (if (isPkgLine) "\n" else "")  + importsAsSrc(indent) + (if (isEmptyLine) "\n" else "\n\n"))
     }
 
     /*
@@ -234,10 +248,10 @@ trait TreeTransformations extends Transformations with TreeFactory {
 
     val addImportStatement = once(insertLocation) &> transformation { case _ ⇒
       splitImports(foundPackage, foundPackage.stats) match {
-        // For an empty PackageDef, with no exiting imports but with an Impl that has an annotation, we need to
+        // For an empty PackageDef, with no existing imports but with an Impl that has an annotation, we need to
         // modify positions so that the annotation, which is not in the AST, doesn't get assigned to the PackageDef
         // but to the Impl
-        case (p @ PackageDef(Ident(nme.EMPTY_PACKAGE_NAME), _), Nil, others @ (impl :: _)) if !impl.symbol.annotations.isEmpty ⇒
+        case (p @ PackageDef(Ident(nme.EMPTY_PACKAGE_NAME), _), Nil, others @ (impl :: _)) ⇒
           // The empty PackageDef starts at the position of its first child, so the annotation of the Impl
           // is outside its parent's range. The Source Generator can't handle this, so we let the PackageDef
           // start at position 0 so that the annotation gets associated to the child.
@@ -246,8 +260,13 @@ trait TreeTransformations extends Transformations with TreeFactory {
         case (p, imports, others) ⇒
           if (imports.nonEmpty)
             insertAfter(imports.last.pos)
-          else
-            insertBefore(others.head.pos)
+          else p.stats match {
+            case po +: _ if po.symbol.isPackageObject ⇒
+              val pos = p.pos withStart 0
+              insertBefore(pos)
+            case _ ⇒
+              insertAfterPackage(p.pos)
+          }
       }
     }
 
