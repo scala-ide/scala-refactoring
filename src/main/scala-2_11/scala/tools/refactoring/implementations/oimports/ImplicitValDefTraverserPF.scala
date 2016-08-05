1099601731d66b967e7d52e17084e67bbf352530
wpopielarski <wpopielarski@gmail.com>
2016-08-05 09:58:40 +0200

    Fixes simple scenario with implicits built with macro

--- src/main/scala-2_11/scala/tools/refactoring/implementations/oimports/ImplicitValDefTraverserPF.scala
+++ src/main/scala-2_11/scala/tools/refactoring/implementations/oimports/ImplicitValDefTraverserPF.scala
@@ -7,6 +7,11 @@ class ImplicitValDefTraverserPF[O <: OrganizeImports](val oiInstance: O) {
   import oiInstance.global._
   import oiInstance.global.analyzer._
 
+  private def continueWithFunction(traverser: Traverser, rhs: Attachable) = rhs match {
+    case TypeApply(fun, _) =>
+      traverser.traverse(fun)
+  }
+
   def apply(traverser: Traverser): PartialFunction[Tree, Unit] = {
     case ValDef(_, _, _, rhs: Attachable) if rhs.hasAttachment[MacroExpansionAttachment] =>
       val mea = rhs.attachments.get[MacroExpansionAttachment]
@@ -16,5 +21,6 @@ class ImplicitValDefTraverserPF[O <: OrganizeImports](val oiInstance: O) {
       }.foreach { expanded =>
         traverser.traverse(expanded.expr)
       }
+      continueWithFunction(traverser, rhs)
   }
 }
