bf17c45b603cd4c2cb615ba84eadf6d6e608d018
wpopielarski <wpopielarski@gmail.com>
2017-08-14 14:42:05 +0200

    Adds tests for default import grouping.

--- src/test/scala/scala/tools/refactoring/tests/implementations/imports/OrganizeImportsGroupsTest.scala
+++ src/test/scala/scala/tools/refactoring/tests/implementations/imports/OrganizeImportsGroupsTest.scala
@@ -161,4 +161,223 @@ class OrganizeImportsGroupsTest extends OrganizeImportsBaseTest {
       }
     """
   } applyRefactoring organize(List("javava", "sca"))
+
+  @Test
+  def defaultGroupInTheMiddleVersion1() = new FileSet {
+    source becomes
+    """
+      import org.xml.sax.Attributes
+
+      import java.util.AbstractList
+      import java.util.BitSet
+      import scala.io.Source
+
+      import scala.collection.mutable.HashMap
+      import scala.collection.mutable.ListBuffer
+
+      trait Temp {
+        // we need some code that use the imports
+        val x: (ListBuffer[Int], HashMap[String, Int])
+        val y: (AbstractList[Int], BitSet)
+        val z: (Attributes, Source)
+      }
+    """
+  } applyRefactoring organize(List("org.xml", "*", "scala.collection"))
+
+  @Test
+  def defaultGroupInTheMiddleVersion2() = new FileSet {
+    source becomes
+    """
+      import org.xml.sax.Attributes
+
+      import java.util.AbstractList
+      import java.util.BitSet
+
+      import scala.io.Source
+
+      import scala.collection.mutable.HashMap
+      import scala.collection.mutable.ListBuffer
+
+      trait Temp {
+        // we need some code that use the imports
+        val x: (ListBuffer[Int], HashMap[String, Int])
+        val y: (AbstractList[Int], BitSet)
+        val z: (Attributes, Source)
+      }
+    """
+  } applyRefactoring organize(List("org.xml", "java", "*", "scala.collection"))
+
+  @Test
+  def defaultGroupInTheMiddleButEmpty() = new FileSet {
+    source becomes
+    """
+      import org.xml.sax.Attributes
+
+      import java.util.AbstractList
+      import java.util.BitSet
+
+      import scala.io.Source
+      import scala.collection.mutable.HashMap
+      import scala.collection.mutable.ListBuffer
+
+      trait Temp {
+        // we need some code that use the imports
+        val x: (ListBuffer[Int], HashMap[String, Int])
+        val y: (AbstractList[Int], BitSet)
+        val z: (Attributes, Source)
+      }
+    """
+  } applyRefactoring organize(List("org", "java", "*", "scala"))
+
+  @Test
+  def defaultGroupInTheBeginningVersion1() = new FileSet {
+    source becomes
+    """
+      import java.util.AbstractList
+      import java.util.BitSet
+      import scala.io.Source
+
+      import org.xml.sax.Attributes
+
+      import scala.collection.mutable.HashMap
+      import scala.collection.mutable.ListBuffer
+
+      trait Temp {
+        // we need some code that use the imports
+        val x: (ListBuffer[Int], HashMap[String, Int])
+        val y: (AbstractList[Int], BitSet)
+        val z: (Attributes, Source)
+      }
+    """
+  } applyRefactoring organize(List("*", "org.xml", "scala.collection"))
+
+  @Test
+  def defaultGroupInTheBeginningVersion2() = new FileSet {
+    source becomes
+    """
+      import org.xml.sax.Attributes
+
+      import scala.collection.mutable.HashMap
+      import scala.collection.mutable.ListBuffer
+      import scala.io.Source
+
+      import java.util.AbstractList
+      import java.util.BitSet
+
+      trait Temp {
+        // we need some code that use the imports
+        val x: (ListBuffer[Int], HashMap[String, Int])
+        val y: (AbstractList[Int], BitSet)
+        val z: (Attributes, Source)
+      }
+    """
+  } applyRefactoring organize(List("*", "scala", "java"))
+
+  @Test
+  def defaultGroupInTheBeginningButEmpty() = new FileSet {
+    source becomes
+    """
+      import org.xml.sax.Attributes
+
+      import scala.collection.mutable.HashMap
+      import scala.collection.mutable.ListBuffer
+      import scala.io.Source
+
+      import java.util.AbstractList
+      import java.util.BitSet
+
+      trait Temp {
+        // we need some code that use the imports
+        val x: (ListBuffer[Int], HashMap[String, Int])
+        val y: (AbstractList[Int], BitSet)
+        val z: (Attributes, Source)
+      }
+    """
+  } applyRefactoring organize(List("*", "org", "scala", "java"))
+
+  @Test
+  def noGroupingIsDefaultGrouping() = new FileSet {
+    source becomes
+    """
+      import java.util.AbstractList
+      import java.util.BitSet
+      import org.xml.sax.Attributes
+      import scala.collection.mutable.HashMap
+      import scala.collection.mutable.ListBuffer
+      import scala.io.Source
+
+      trait Temp {
+        // we need some code that use the imports
+        val x: (ListBuffer[Int], HashMap[String, Int])
+        val y: (AbstractList[Int], BitSet)
+        val z: (Attributes, Source)
+      }
+    """
+  } applyRefactoring organize(List("*"))
+
+  @Test
+  def defaultGroupInTheEndVersion1() = new FileSet {
+    source becomes
+    """
+      import org.xml.sax.Attributes
+
+      import scala.collection.mutable.HashMap
+      import scala.collection.mutable.ListBuffer
+      import scala.io.Source
+
+      import java.util.AbstractList
+      import java.util.BitSet
+
+      trait Temp {
+        // we need some code that use the imports
+        val x: (ListBuffer[Int], HashMap[String, Int])
+        val y: (AbstractList[Int], BitSet)
+        val z: (Attributes, Source)
+      }
+    """
+  } applyRefactoring organize(List("org", "scala", "*"))
+
+  @Test
+  def defaultGroupInTheEndVersion2() = new FileSet {
+    source becomes
+    """
+      import scala.collection.mutable.HashMap
+      import scala.collection.mutable.ListBuffer
+      import scala.io.Source
+
+      import java.util.AbstractList
+      import java.util.BitSet
+
+      import org.xml.sax.Attributes
+
+      trait Temp {
+        // we need some code that use the imports
+        val x: (ListBuffer[Int], HashMap[String, Int])
+        val y: (AbstractList[Int], BitSet)
+        val z: (Attributes, Source)
+      }
+    """
+  } applyRefactoring organize(List("scala", "java", "*"))
+
+  @Test
+  def defaultGroupInTheEndButEmpty() = new FileSet {
+    source becomes
+    """
+      import org.xml.sax.Attributes
+
+      import scala.collection.mutable.HashMap
+      import scala.collection.mutable.ListBuffer
+      import scala.io.Source
+
+      import java.util.AbstractList
+      import java.util.BitSet
+
+      trait Temp {
+        // we need some code that use the imports
+        val x: (ListBuffer[Int], HashMap[String, Int])
+        val y: (AbstractList[Int], BitSet)
+        val z: (Attributes, Source)
+      }
+    """
+  } applyRefactoring organize(List("org", "scala", "java", "*"))
 }
