c083049b218b0e7431fd0e6885e5eb041b9816ae
Pablo Fco. Pérez Hidalgo <contact@pablofranciscoperez.info>
2017-09-23 14:07:16 +0200

    Improved tests

--- src/test/scala/scala/tools/refactoring/tests/implementations/ImplementMethodsTest.scala
+++ src/test/scala/scala/tools/refactoring/tests/implementations/ImplementMethodsTest.scala
@@ -7,12 +7,95 @@ import scala.tools.refactoring.tests.util.{TestHelper, TestRefactoring}
 class ImplementMethodsTest extends TestHelper with TestRefactoring {
   outer =>
 
-  val fromSource =
+  def implementMethods(pro: FileSet) = new TestRefactoringImpl(pro) {
+    override val refactoring = new ImplementMethods with TestProjectIndex
+    val changes = performRefactoring(())
+  }.changes
+
+  @Test
+  def implementMethodFromFirstMixing() = new FileSet() {
     """
       |package implementMethods
       |
       |trait T {
       |  def f(x: Int): String
+      |}
+      |
+      |trait S {
+      |  def g(x: Int): Int
+      |}
+      |
+      |object Obj extends /*(*/T/*)*/ with S {
+      |  val x: Int = ???
+      |}
+    """.stripMargin becomes
+    """
+      |package implementMethods
+      |
+      |trait T {
+      |  def f(x: Int): String
+      |}
+      |
+      |trait S {
+      |  def g(x: Int): Int
+      |}
+      |
+      |object Obj extends /*(*/T/*)*/ with S {
+      |  val x: Int = ???
+      |
+      |  def f(x: Int): String = {
+      |    ???
+      |  }
+      |}
+    """.stripMargin
+  } applyRefactoring implementMethods
+
+  @Test
+  def implementMethodFromSecondMixing() = new FileSet() {
+    """
+      |package implementMethods
+      |
+      |trait T {
+      |  def f(x: Int): String
+      |}
+      |
+      |trait S {
+      |  def g(x: Int): Int
+      |}
+      |
+      |object Obj extends T with /*(*/S/*)*/ {
+      |  val x: Int = ???
+      |}
+    """.stripMargin becomes
+    """
+      |package implementMethods
+      |
+      |trait T {
+      |  def f(x: Int): String
+      |}
+      |
+      |trait S {
+      |  def g(x: Int): Int
+      |}
+      |
+      |object Obj extends T with /*(*/S/*)*/ {
+      |  val x: Int = ???
+      |
+      |  def g(x: Int): Int = {
+      |    ???
+      |  }
+      |}
+    """.stripMargin
+  } applyRefactoring implementMethods
+
+  @Test
+  def implementMethodsNotImplemented() = new FileSet() {
+    """
+      |package implementMethods
+      |
+      |trait T {
+      |  def f(x: Int): String
+      |  def g(x: Int): Int
       |}
       |
       |object Obj extends /*(*/T/*)*/ {
@@ -20,14 +103,13 @@ class ImplementMethodsTest extends TestHelper with TestRefactoring {
       |  def g(x: Int): Int = 1
       |
       |}
-    """.stripMargin
-
-  val toSource =
+    """.stripMargin becomes
     """
       |package implementMethods
       |
       |trait T {
       |  def f(x: Int): String
+      |  def g(x: Int): Int
       |}
       |
       |object Obj extends /*(*/T/*)*/ {
@@ -40,15 +122,6 @@ class ImplementMethodsTest extends TestHelper with TestRefactoring {
       |
       |}
     """.stripMargin
-
-  def implementMethods(pro: FileSet) = new TestRefactoringImpl(pro) {
-    override val refactoring = new ImplementMethods with TestProjectIndex
-    val changes = performRefactoring(())
-  }.changes
-
-  @Test
-  def doRefactor() = new FileSet() {
-    fromSource becomes toSource
   } applyRefactoring implementMethods
 
 }
